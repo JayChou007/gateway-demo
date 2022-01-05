5fdcbfc05e6e147523d8a16dae4bd6c5c4626947
kevin <jay15979616825@163.com>
2022-01-05 20:38:53 +0800

    gateway docking apollo

--- ruoyi-gateway/src/main/java/com/ruoyi/gateway/RuoyiGatewayApp.java
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/RuoyiGatewayApp.java
@@ -1,5 +1,6 @@
 package com.ruoyi.gateway;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -7,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableApolloConfig
 public class RuoyiGatewayApp
 {
     public static void main(String[] args)
